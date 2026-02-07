1e2e4b4b9c7a95897a44316c1990d990cf4577c4
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-02-07 22:30:01 +0530

    Two State Binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
 import React from "react";
-
+import { useState } from "react";
 const App = () => {
-  function submitHandler(e){
+  const [name, setName] = useState("");
+  const [allUser, setAllUser] = useState([]);
+  function submitHandler(e) {
     e.preventDefault();
-    console.log("Form submitted");
+    console.log(name);
+
   }
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-gray-500">
-      <form onSubmit={(e) =>{submitHandler(e)}} className="flex flex-col gap-2 ">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+          let oldUser = [...allUser];
+          oldUser.push(name);
+          setAllUser(oldUser);
+          setName("");
+        }}
+        className="flex flex-col gap-2"
+      >
         <input
           type="text"
           className="px-5 py-3 bg-gray-600 rounded-2xl"
           placeholder="Enter your meow"
+          value={name}
+          required
+          onChange={(e) => {
+            // console.log(e.target.value);
+            setName(e.target.value);
+          }}
         />
         <button className="text-md rounded-xl">Submit</button>
       </form>
+      {
+        allUser.map((user, index) => {
+          return <h1 key={index}>{user}</h1>
+        })
+      }
     </div>
   );
 };
